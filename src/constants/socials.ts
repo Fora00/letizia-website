c1d520c86d20128ce148a9612ee925255f81a5b3
Fora00 <franzmito@gmail.com>
2026-01-23 19:20:51 +0100

    some const logic

---
+++ src/constants/socials.ts
@@ -0,0 +1,6 @@
+export const social = {
+  mail: 'letiziamancini.chinesiologa@gmail.com',
+  facebook: 'https://www.facebook.com/profile.php?id=61555750393500',
+  linkedin: 'https://www.linkedin.com/in/letizia-mancini-8a6163242',
+  instagram: 'https://www.instagram.com/letiziamancini_chinesiologa',
+};
