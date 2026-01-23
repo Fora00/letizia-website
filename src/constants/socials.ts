b7e154b736634d76bc2170429c14a2a9f8453917
Fora00 <franzmito@gmail.com>
2026-01-23 19:32:45 +0100

    some ux improvement

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -3,4 +3,5 @@ export const social = {
   facebook: 'https://www.facebook.com/profile.php?id=61555750393500',
   linkedin: 'https://www.linkedin.com/in/letizia-mancini-8a6163242',
   instagram: 'https://www.instagram.com/letiziamancini_chinesiologa',
+  maps: 'https://maps.app.goo.gl/YDsDdcLEUQq7Ajej8',
 };
