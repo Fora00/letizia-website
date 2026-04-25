cb7598a3dde0ac69d626223a5268e14f6bc1c14c
Fora00 <franzmito@gmail.com>
2026-04-25 09:23:18 +0200

    fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 
 export default defineConfig({
   site: isGitHubPages ? 'https://fora00.github.io' : 'https://letiziamancini.it',
-  ...(isGitHubPages ? { base: '/letizia-website' } : {}),
+  ...(isGitHubPages ? { base: '/letizia-website/' } : {}),
   output: 'static',
   server: {
     port: 3000
